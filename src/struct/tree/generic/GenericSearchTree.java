1c11e0c58bc6629a0b9b7fae67db823229ea5c11
54754N4 <35555527+54754N4@users.noreply.github.com>
2019-08-03 19:38:08 +0700

    Fixed GenericSearchTree (still needs search methods though)

--- src/struct/tree/generic/GenericSearchTree.java
+++ src/struct/tree/generic/GenericSearchTree.java
@@ -1,5 +1,6 @@
 package struct.tree.generic;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import struct.contract.GenericSearchTreeContract;
@@ -9,12 +10,24 @@ public class GenericSearchTree<K, V> implements GenericSearchTreeContract<K, V> 
 	private Node<K, V> root;
 	
 	public static void main(String[] args) {
-		GenericSearchTree<Integer, String> tree = new GenericSearchTree<>();
-		tree.add(new Node<>());
+		Integer[] ints = {0,1,2,3,4,5,6,7};
+		String[] strs = {"s0","s1","s2","s3","s4","s5","s6","s7"};
+		List<Node<Integer, String>> nodes = new ArrayList<>();
+		for (int i=0; i<ints.length; i++) nodes.add(new Node<>(ints[i], strs[i]));
+		nodes.get(0).add(nodes.get(1)).add(nodes.get(2));
+		nodes.get(2).add(nodes.get(4));
+		nodes.get(1).add(nodes.get(3)).add(nodes.get(5)).add(nodes.get(6));
+		nodes.get(3).add(nodes.get(7));
+		GenericSearchTree<Integer, String> tree = new GenericSearchTree<>(nodes.get(0));
+		System.out.println(tree);
+	}
+	
+	public GenericSearchTree(Node<K, V> root) { 
+		this.root = root;
 	}
 	
 	public GenericSearchTree() {
-		root = new Node<>();
+		this(new Node<>());
 	}
 
 	@Override
@@ -25,5 +38,25 @@ public class GenericSearchTree<K, V> implements GenericSearchTreeContract<K, V> 
 	@Override
 	public List<Node<K, V>> getChildren() {
 		return root.getChildren();
+	}
+
+	@Override
+	public GenericTreeContract<K, V> add(K key, V value) {
+		return root.add(key, value);
+	}
+
+	@Override
+	public K getKey() {
+		return root.getKey();
+	}
+
+	@Override
+	public V getValue() {
+		return root.getValue();
+	}
+	
+	@Override
+	public String toString() {
+		return root.toString();
 	}
 }
--- src/struct/tree/generic/Node.java
+++ src/struct/tree/generic/Node.java
@@ -7,9 +7,17 @@ import struct.contract.GenericSearchTreeContract;
 import struct.contract.GenericTreeContract;
 
 public class Node<K, V> implements GenericSearchTreeContract<K, V> {
+	private K key;
+	private V value;
 	private List<Node<K, V>> children;
 	
 	public Node() {
+		this(null, null);
+	}
+	
+	public Node(K key, V value) {
+		this.setKey(key);
+		this.setValue(value);
 		children = new ArrayList<>();
 	}
 	
@@ -20,7 +28,36 @@ public class Node<K, V> implements GenericSearchTreeContract<K, V> {
 	}
 	
 	@Override
+	public Node<K, V> add(K key, V value) {
+		children.add(new Node<>(key, value));
+		return this;
+	}
+	
+	@Override
 	public List<Node<K, V>> getChildren() {
 		return children;
+	}
+
+	@Override
+	public K getKey() {
+		return key;
+	}
+
+	@Override
+	public V getValue() {
+		return value;
+	}
+	
+	@Override
+	public String toString() {
+		return simpleRepresentation();
+	}
+	
+	public void setKey(K key) {
+		this.key = key;
+	}
+
+	public void setValue(V value) {
+		this.value = value;
 	}
 }
