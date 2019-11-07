ad621f3f0ad1ce7bf3a150e8a2ac3b48ff423b75
54754N4 <35555527+54754N4@users.noreply.github.com>
2019-11-07 20:21:39 +0700

    Suppressed cast since it isnt an issue in this case

--- src/logic/NestedIndexedVisitor.java
+++ src/logic/NestedIndexedVisitor.java
@@ -7,6 +7,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
+@SuppressWarnings("unchecked")
 public class NestedIndexedVisitor<K> {	// multidimensional list looper
 	private int dimension;
 	private Collection<? extends Collection<?>> collection;
