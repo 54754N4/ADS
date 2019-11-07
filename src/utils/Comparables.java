75e77ca435397dc550925fb30e40072b15afad41
54754N4 <35555527+54754N4@users.noreply.github.com>
2019-11-07 20:35:42 +0700

    Added indexOfUltimum to comparison utils

--- src/utils/Comparables.java
+++ src/utils/Comparables.java
@@ -2,10 +2,23 @@ package utils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public abstract class Comparables {
 
+	public static <K> int indexOfUltimum(List<K> values, Comparator<K> c) {
+		int index = 0;
+		K ultimum = values.get(0);
+		for (int i=0; i<values.size(); i++) {
+			if (c.compare(values.get(i), ultimum) < 0) {
+				ultimum = values.get(i);
+				index = i;
+			}
+		}
+		return index;
+	}
+	
 	public static <K extends Comparable<K>> K max(List<K> values) {
 		K max = values.get(0);
 		for (K value : values) if (value.compareTo(max) > 0) max = value;
@@ -15,6 +28,7 @@ public abstract class Comparables {
 	public static void main(String[] args) {
 		List<Integer> list = new ArrayList<>(Arrays.asList(41,62,34,24,35,64,87));
 		System.out.println(max(list));
+		System.out.println(list.get(indexOfUltimum(list, Comparator.<Integer>reverseOrder())));
 	}
 
 }
